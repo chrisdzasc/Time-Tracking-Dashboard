1b1d1ca0ce45eb578e8ff28e22a3530562432792
chrisdzasc <christian.dzasc@gmail.com>
2026-03-03 19:30:10 -0600

    feat: add span tags and DOM selectors for all activity hours

--- js/app.js
+++ js/app.js
@@ -2,6 +2,19 @@ const daily = document.querySelector('#daily');
 const weekly = document.querySelector('#weekly');
 const monthly = document.querySelector('#monthly');
 
+const workCurrent       = document.querySelector('#work-current');
+const workPrevious      = document.querySelector('#work-previous');
+const playCurrent       = document.querySelector('#play-current');
+const playPrevious      = document.querySelector('#play-previous');
+const studyCurrent      = document.querySelector('#study-current');
+const studyPrevious     = document.querySelector('#study-previous');
+const exerciseCurrent   = document.querySelector('#exercise-current');
+const exercisePrevious  = document.querySelector('#exercise-previous');
+const socialCurrent     = document.querySelector('#social-current');
+const socialPrevious    = document.querySelector('#social-previous');
+const selfCareCurrent   = document.querySelector('#self-care-current');
+const selfCarePrevious  = document.querySelector('#self-care-previous');
+
 const buttons = [daily, weekly, monthly];
 
 function removeActiveClasses() {
